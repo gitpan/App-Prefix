aff31889dbc9910fcc24d4fc24130b15ac4c929d
Josh Rabinowitz <joshr-cpan@joshr.com>
2013-10-14 17:58:50 +0000

    Import of JOSHR/App-Prefix-0.08 from CPAN.
    gitpan-cpan-distribution: App-Prefix
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         JOSHR/App-Prefix-0.08.tar.gz
    gitpan-cpan-author:       JOSHR
    gitpan-cpan-maturity:     released

---
+++ lib/App/Prefix.pm
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+package App::Prefix;
+
+our $VERSION = '0.08'; 
+
+# ABSTRACT: provides 'prefix' filter, which prepends data to lines passed
+#
+# NOTE: main file for this distribution is bin/prefix
+# 
+#  Copyright 2010-2013 Josh Rabinowitz.
+#
+# 1;
